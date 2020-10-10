8a3911098f06ca33d0b795e2878bb091c449993d
usbport11 <51235513+usbport11@users.noreply.github.com>
2020-10-10 13:05:37 +0300

    Update HelloWorldScene.h

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -12,6 +12,8 @@ public:
 	static cocos2d::Scene* createScene();
 	virtual bool init();
 	void menuCloseCallback(cocos2d::Ref* pSender);
+	void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+	void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
 	CREATE_FUNC(HelloWorld);
 };
 
